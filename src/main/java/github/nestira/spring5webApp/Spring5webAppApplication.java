67ff1da909731fdeb253c625f29a55848a5915a8
Nestira <JingerPupa@gmail.com>
2018-03-02 10:05:13 -0500

    add config.xml to resources

--- src/main/java/github/nestira/spring5webApp/Spring5webAppApplication.java
+++ src/main/java/github/nestira/spring5webApp/Spring5webAppApplication.java
@@ -2,8 +2,10 @@ package github.nestira.spring5webApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:config.xml")
 public class Spring5webAppApplication {
 
 	public static void main(String[] args) {
