22568dfd9eecd97d41f7346799094af5506ea6d2
Nestira <JingerPupa@gmail.com>
2018-04-12 17:23:53 -0400

    spring boot upgraded to 2.0.1

--- src/main/java/github/nestira/spring5webApp/Spring5webAppApplication.java
+++ src/main/java/github/nestira/spring5webApp/Spring5webAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:config.xml")
+//@ImportResource("classpath:config.xml")
 public class Spring5webAppApplication {
 
 	public static void main(String[] args) {
