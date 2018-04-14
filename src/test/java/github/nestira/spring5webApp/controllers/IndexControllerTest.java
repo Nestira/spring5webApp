03bd5b3f85fab0c29f027216f519e825d23d33eb
Nestira <JingerPupa@gmail.com>
2018-04-14 05:05:50 -0400

    added Argument Capture test method

--- src/test/java/github/nestira/spring5webApp/controllers/IndexControllerTest.java
+++ src/test/java/github/nestira/spring5webApp/controllers/IndexControllerTest.java
@@ -1,15 +1,22 @@
 package github.nestira.spring5webApp.controllers;
 
+import github.nestira.spring5webApp.models.Recipe;
 import github.nestira.spring5webApp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class IndexControllerTest {
 
@@ -31,9 +38,24 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() {
 
+        //Given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setDescription("This is a description");
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+
         String templateName = controller.getIndexPage(model);
 
         assertEquals("index", templateName);
         verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        assertEquals(recipes.size(), argumentCaptor.getValue().size());
     }
 }
