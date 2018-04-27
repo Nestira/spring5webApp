7064928eb8a519b9ddf0b7f913d0a76fd2e404b4
Nestira <JingerPupa@gmail.com>
2018-04-27 07:57:51 -0400

    no header boilerplate code support

---
+++ src/main/java/github/nestira/spring5webApp/config/thymeleafTemplateEngineService.java
@@ -0,0 +1,11 @@
+package github.nestira.spring5webApp.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+@Configuration
+public class thymeleafTemplateEngineService {
+
+}
