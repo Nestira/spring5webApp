97149c06b3a76d9f7cf53514af6c38d7113d4570
Nestira <JingerPupa@gmail.com>
2018-06-06 08:00:19 -0400

    add new recipe form

--- src/main/java/github/nestira/spring5webApp/controllers/RecipeController.java
+++ src/main/java/github/nestira/spring5webApp/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package github.nestira.spring5webApp.controllers;
 
+import github.nestira.spring5webApp.commands.RecipeCommand;
 import github.nestira.spring5webApp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -22,5 +25,21 @@ public class RecipeController {
 
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping({"/recipe/new"})
+    public String newRecipe(Model model) {
+
+        model.addAttribute("recipe", new RecipeCommand()); //"recipe" match object name in html.
+        return "recipe/recipeform";
+    }
+
+    @PostMapping({"/recipe/"}) //match "th:action" in the form or object name? Maybe it has to match both
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+
+        log.debug(command.getDescription());
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
